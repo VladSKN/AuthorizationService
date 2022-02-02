7f1dc9234d4002e0edcef1fbe7d4f21d6aecd365
Vlad Semikin <vladsemikin3@gmail.com>
2022-02-02 15:09:04 +0200

    nginx_proxy add

--- src/main/java/ru/netology/authorizationservice/controller/AuthorizationController.java
+++ src/main/java/ru/netology/authorizationservice/controller/AuthorizationController.java
@@ -3,6 +3,7 @@ package ru.netology.authorizationservice.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.authorizationservice.authorities.Authorities;
