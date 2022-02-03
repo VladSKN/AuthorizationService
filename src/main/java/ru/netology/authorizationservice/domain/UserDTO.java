4c6453d0c1857b1cf16b81f9a05053eb9ac88997
Vlad Semikin <vladsemikin3@gmail.com>
2022-02-03 22:52:56 +0200

    dockerfile add and switch java version 11

--- src/main/java/ru/netology/authorizationservice/domain/UserDTO.java
+++ src/main/java/ru/netology/authorizationservice/domain/UserDTO.java
@@ -1,3 +1,19 @@
 package ru.netology.authorizationservice.domain;
 
-public record UserDTO(String name, String password) { }
+public class UserDTO {
+    private final String name;
+    private final String password;
+
+    public UserDTO(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
