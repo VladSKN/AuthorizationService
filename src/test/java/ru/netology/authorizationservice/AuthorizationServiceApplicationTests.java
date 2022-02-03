79a5c253502b669a09d00460ba01120b2f7042af
Vlad Semikin <vladsemikin3@gmail.com>
2022-02-03 18:21:19 +0200

    add getUserAuthoritiesTest

--- src/test/java/ru/netology/authorizationservice/AuthorizationServiceApplicationTests.java
+++ src/test/java/ru/netology/authorizationservice/AuthorizationServiceApplicationTests.java
@@ -1,12 +1,26 @@
 package ru.netology.authorizationservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import ru.netology.authorizationservice.authorities.Authorities;
+import ru.netology.authorizationservice.repository.UserRepository;
+import ru.netology.authorizationservice.service.AuthorizationService;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
 class AuthorizationServiceApplicationTests {
+    private final UserRepository userRepository = mock(UserRepository.class);
+    private final AuthorizationService service = new AuthorizationService(userRepository);
+
 
     @Test
-    void contextLoads() {
+    void getUserAuthoritiesTest() {
+        String name = "admin";
+        String password = "password";
+        when(userRepository.getUserAuthorities(name, password))
+                .thenReturn(List.of(Authorities.DELETE, Authorities.READ, Authorities.WRITE));
     }
 }
